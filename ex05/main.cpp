52de462da360cc5a96b28e7854efea93d33c6387
Ravaillack <matkowski.lucas@gmail.com>
2025-05-11 20:40:12 +0200

    ex05 fini

--- ex05/main.cpp
+++ ex05/main.cpp
@@ -6,8 +6,24 @@
 /*   By: Lmatkows <lmatkows@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/11 18:50:41 by Lmatkows          #+#    #+#             */
-/*   Updated: 2025/05/11 18:50:53 by Lmatkows         ###   ########.fr       */
+/*   Updated: 2025/05/11 20:39:17 by Lmatkows         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Harl.hpp"
+
+int	main(void)
+{
+	Harl Harold;
+
+	std::cout << "j'essaie avec Hello" << std::endl;
+	Harold.complain("Hello");
+	std::cout << "j'essaie avec INFO" << std::endl;
+	Harold.complain("INFO");
+	std::cout << "j'essaie avec ERROR" << std::endl;
+	Harold.complain("ERROR");
+	std::cout << "j'essaie avec DEBUG" << std::endl;
+	Harold.complain("DEBUG");
+	std::cout << "j'essaie avec WARNING" << std::endl;
+	Harold.complain("WARNING");
+}
