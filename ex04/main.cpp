c0aa26b5da89e75ddea790142414710f92aac3b4
Ravaillack <matkowski.lucas@gmail.com>
2025-05-11 21:17:44 +0200

    version revue et corrigee

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: Lmatkows <lmatkows@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/10 12:48:54 by lmatkows          #+#    #+#             */
-/*   Updated: 2025/05/11 18:46:48 by Lmatkows         ###   ########.fr       */
+/*   Updated: 2025/05/11 21:14:50 by Lmatkows         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int	main(int argc, char **argv)
 {
 	if (argc != 4)
 	{
-		std::cerr << "There must be 3 inputs : ./src string_to_replace new_string" << std::endl;
+		std::cerr << "There must be 3 inputs : <file> <string_to_replace> <new_string>" << std::endl;
 		return (1);
 	}
 	ReadFile	inst_rf(argv[1]);
