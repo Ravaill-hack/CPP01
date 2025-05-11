cb1680db40ed5c1cfaf21a2d93795f9286087cf5
Ravaillack <matkowski.lucas@gmail.com>
2025-05-11 18:48:37 +0200

    version finale

--- ex04/utils.cpp
+++ ex04/utils.cpp
@@ -1,28 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
+/*   utils.cpp                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: Lmatkows <lmatkows@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/05/10 12:48:54 by lmatkows          #+#    #+#             */
-/*   Updated: 2025/05/11 18:46:48 by Lmatkows         ###   ########.fr       */
+/*   Created: 2025/05/11 15:22:19 by Lmatkows          #+#    #+#             */
+/*   Updated: 2025/05/11 15:22:20 by Lmatkows         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ReadFile.hpp"
-#include "WriteFile.hpp"
-
-int	main(int argc, char **argv)
-{
-	if (argc != 4)
-	{
-		std::cerr << "There must be 3 inputs : ./src string_to_replace new_string" << std::endl;
-		return (1);
-	}
-	ReadFile	inst_rf(argv[1]);
-	WriteFile	inst_wf(argv[1], argv[2], argv[3]);
-	if (!inst_rf.try_to_read() || !inst_wf.try_to_write(inst_rf.instream))
-		return (1);
-	return (0);
-}
